2c6f4ad9f07eb510bd0bfbca82cb47780a2e0a27
senharo1981 <senharo1981@gmail.com>
2026-02-14 23:36:27 +0500

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import TailoringApp from './TailoringApp';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error("Could not find root element");
+const App: React.FC = () => {
+  return (
+    <div className="min-h-screen">
+      <TailoringApp />
+    </div>
+  );
+};
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default App;
